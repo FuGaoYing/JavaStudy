ecc89fa5e1e693741db00e47186b93d9322d24fa
付高营 <fugaoying@utry.cn>
2023-03-06 18:31:13 +0800

    Initial commit

---
+++ rabbitMqTest/src/main/java/com/my/study/RabbitmqMyTest.java
@@ -0,0 +1,17 @@
+package com.my.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: FGY
+ * @Description:
+ * @Date: Created in 2023/3/6 18:26
+ * @Version:
+ */
+@SpringBootApplication
+public class RabbitmqMyTest {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqMyTest.class,args);
+    }
+}
